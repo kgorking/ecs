a745216eab3d55f216c72079886c17a68ff74ccb
Kenneth Gorking <dasapfe@yahoo.dk>
2019-07-10 00:22:58 +0200

    Fixed a faulty unit test

--- unittest/component deletion.cpp
+++ unittest/component deletion.cpp
@@ -7,12 +7,12 @@ TEST_CASE("Component deletion")
 	{
 		ecs::runtime::reset();
 
-		// Add a system for the C_Counter component
+		// Add a system
 		ecs::add_system([](ecs::entity_id id, unsigned const& c) {
 			REQUIRE(id.id == c);
 		});
 
-		// Create some entities and add a C_Counter component initialized to its index
+		// Create some entities initialized to their index
 		// 0 1 2 3 4 5 6 7 8 9 10
 		for (unsigned e = 0; e <= 10; e++)
 			ecs::add_component(e, e);
@@ -27,6 +27,7 @@ TEST_CASE("Component deletion")
 		REQUIRE(11ull == ecs::get_component_count<unsigned>());
 
 		// Remove some components
+		// 0 1 2 3 4 10
 		for (unsigned e = 9; e >= 5; --e)
 			ecs::remove_component<unsigned>(e);
 		ecs::update_systems();
@@ -35,11 +36,12 @@ TEST_CASE("Component deletion")
 		REQUIRE(6ull == ecs::get_component_count<unsigned>());
 
 		// re-insert new stuff
+		// 0 1 2 3 4 6 7 8 9 10
 		for (unsigned e = 6; e <= 9; e++)
 			ecs::add_component(e, e);
 		ecs::update_systems();
 
 		// There should now be 10 components
-		REQUIRE(9ull == ecs::get_component_count<unsigned>());
+		REQUIRE(10ull == ecs::get_component_count<unsigned>());
 	}
 }
