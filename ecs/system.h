39e7e6301ffcc3774c633306fbc49b1bb45210db
Kenneth Gorking <dasapfe@yahoo.dk>
2019-07-25 17:25:27 +0200

    Made a public function private.

--- ecs/system.h
+++ ecs/system.h
@@ -2,20 +2,24 @@
 
 namespace ecs
 {
+	//extern void commit_changes();
+
 	class system
 	{
-	public:
-		// Run this system on all of its associated components
-		virtual void update() = 0;
+		friend void commit_changes();
 
 		// Process changes to component layouts
 		virtual void process_changes() = 0;
 
 	public:
+		// Run this system on all of its associated components
+		virtual void update() = 0;
+
+	public:
 		system() = default;
+		virtual ~system() = default;
 		system(system const&) = delete;
 		system(system&&) = default;
-		virtual ~system() = default;
 		system& operator =(system const&) = delete;
 		system& operator =(system&&) = default;
 	};
