28f8579dd5b9b8aedda826bb0d054c75d063a296
Kenneth Gorking <dasapfe@yahoo.dk>
2020-02-18 22:35:23 +0100

    Spellchecked

--- include/ecs/component_specifier.h
+++ include/ecs/component_specifier.h
@@ -21,7 +21,7 @@ namespace ecs
 	// Add this in 'ecs_flags()' to mark a component as unmutable.
 	// A compile-time error will be raised if a system tries to
 	// access the component through a non-const reference.
-	struct unmutable {};
+	struct immutable {};
 
 	// Add flags to a component to change its behaviour and memory usage.
 	// Example:
@@ -35,18 +35,18 @@ namespace ecs
 		template <typename T> constexpr auto is_tagged(...)    -> bool { return false; }
 		template <typename T> constexpr auto is_shared(...)    -> bool { return false; }
 		template <typename T> constexpr auto is_transient(...) -> bool { return false; }
-		template <typename T> constexpr auto is_unmutable(...) -> bool { return false; }
+		template <typename T> constexpr auto is_immutable(...) -> bool { return false; }
 
 		template <typename T> constexpr auto is_tagged(int)    -> decltype(typename T::_ecs_flags{}, bool{}) { return std::is_base_of_v<ecs::tag, typename T::_ecs_flags>; }
 		template <typename T> constexpr auto is_shared(int)    -> decltype(typename T::_ecs_flags{}, bool{}) { return std::is_base_of_v<ecs::shared, typename T::_ecs_flags>; }
 		template <typename T> constexpr auto is_transient(int) -> decltype(typename T::_ecs_flags{}, bool{}) { return std::is_base_of_v<ecs::transient, typename T::_ecs_flags>; }
-		template <typename T> constexpr auto is_unmutable(int) -> decltype(typename T::_ecs_flags{}, bool{}) { return std::is_base_of_v<ecs::unmutable, typename T::_ecs_flags>; }
+		template <typename T> constexpr auto is_immutable(int) -> decltype(typename T::_ecs_flags{}, bool{}) { return std::is_base_of_v<ecs::immutable, typename T::_ecs_flags>; }
 
 		// Some helpers
 		template <class T> constexpr static bool is_tagged_v = is_tagged<T>(0);
 		template <class T> constexpr static bool is_shared_v = is_shared<T>(0);
 		template <class T> constexpr static bool is_transient_v = is_transient<T>(0);
-		template <class T> constexpr static bool is_unmutable_v = is_unmutable<T>(0);
+		template <class T> constexpr static bool is_immutable_v = is_immutable<T>(0);
 
 		template <class T> constexpr static bool is_static_v = (is_shared_v<T> || is_tagged_v<T>); // all entities point to the same component
 	}
