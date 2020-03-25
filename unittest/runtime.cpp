84a9e72ad44444d84b70bad94e408a7b0a40edf3
Kenneth Gorking <dasapfe@yahoo.dk>
2020-03-25 19:47:17 +0100

    Moved test into runtime file

--- unittest/runtime.cpp
+++ unittest/runtime.cpp
@@ -41,4 +41,17 @@ TEST_CASE("The runtime interface") {
 		ecs::detail::_context.reset();
 		REQUIRE(runtime_ctr_counter::dtr_count == 1 + 2 + 10);
 	}
+
+	SECTION("allocates storage as needed") {
+		ecs::detail::_context.reset();
+
+		// Use a local struct to avoid it possibly
+		// already existing from another unittest
+		struct S { size_t c; };
+
+		// Add a system-less component to an entity
+		ecs::add_component(0, S{ 0 });
+		ecs::commit_changes();
+		REQUIRE(ecs::get_component_count<S>() == 1);
+	}
 }
