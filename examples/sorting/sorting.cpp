036ffe132ae20508f4a86e5441614bcf50b16610
Kenneth Gorking <dasapfe@yahoo.dk>
2020-05-05 20:46:58 +0200

    Simplified the example to reduce noise

--- examples/sorting/sorting.cpp
+++ examples/sorting/sorting.cpp
@@ -3,7 +3,7 @@
 #include <ecs/ecs.h>
 
 auto constexpr printer = [](int const& i) { std::cout << i << ' '; };
-auto constexpr generator = [](ecs::entity_id) -> int { return 10 + rand() % 89; };
+auto constexpr generator = [](ecs::entity_id) -> int { return rand() % 9; };
 
 auto constexpr sort_asc = [](int const& l, int const& r) { return l < r; };
 auto constexpr sort_des = [](int const& l, int const& r) { return l > r; };
@@ -22,7 +22,7 @@ int main() {
     auto& sys_sort_des = ecs::make_system(printer, sort_des);
     auto& sys_sort_eo = ecs::make_system(printer, sort_even_odd);
 
-    ecs::add_components({0, 19}, generator);
+    ecs::add_components({0, 9}, generator);
     ecs::commit_changes();
 
     sys_no_sort.update();
