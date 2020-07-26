e090c4fb65769c7c5fa45eb4d312bc25006e5405
Kenneth Gorking <dasapfe@yahoo.dk>
2020-07-26 17:58:55 +0200

    Updated sample with life/death. Exposed a bug in range difference finder.

--- benchmark/particles.cpp
+++ benchmark/particles.cpp
@@ -7,14 +7,15 @@
 // https://docs.unrealengine.com/en-US/Resources/ContentExamples/EffectsGallery/2_D/index.html
 // https://docs.unrealengine.com/en-US/Resources/ContentExamples/EffectsGallery/2_E/index.html
 
-constexpr float delta_time = 1.0f / 120.0f;
-constexpr int num_frames = 600;
+constexpr float delta_time = 1.0f / 6.0f;
+constexpr int num_frames = 100;
 constexpr int max_num_particles = 50'000;
 
 struct particle { float x, y; };
 struct color    { float r, g, b; };
 struct velocity { float x, y; };
 struct life     { float val; };
+struct dead     { ecs_flags(ecs::tag); };
 struct gravity  { ecs_flags(ecs::global); float g = 0.2f; };
 
 // Helper lambda to initialize a particle
@@ -47,67 +48,78 @@ auto constexpr life_init = [](ecs::entity_id) -> life {
 	return {0.2f + x*2}; // [0.2, 0.4]
 };
 
+inline std::atomic_int deads = 0;
+void make_systems() {
+    // Apply gravity to the velocity
+    ecs::make_system([](velocity& vel, gravity const& grav) { vel.y -= grav.g * delta_time; });
+
+    // Update a particles position from its velocity
+    ecs::make_system([](particle& par, velocity const& vel) {
+        par.x += vel.x * delta_time;
+        par.y += vel.y * delta_time;
+    });
+
+    // Make sure the particles stay within the bounds.
+    ecs::make_system([](particle& par, velocity& vel) {
+        if (par.x > 1) {
+            par.x = 1;
+            float const p = 2 * vel.x * -1;
+            vel.x = vel.x - p * -1;
+        } else if (par.x < -1) {
+            par.x = -1;
+            float const p = 2 * vel.x * 1;
+            vel.x = vel.x - p * 1;
+        }
+
+        if (par.y > 1) {
+            par.y = 1;
+            float const p = 2 * vel.y * -1;
+            vel.y = vel.y - p * -1;
+        } else if (par.y < -1) {
+            par.y = -1;
+            float const p = 2 * vel.y * 1;
+            vel.y = vel.y - p * 1;
+        }
+    });
+
+    // Paint particles purple if they are in range of (0.0, 0.0)
+    ecs::make_system([](color& col, particle const& par) {
+        float const len_sqr = par.x * par.x + par.y * par.y;
+
+        if (len_sqr > 0.0005f)
+            return; // out of range
+
+        col.r = 1;
+        col.g = 0;
+        col.b = 1;
+    });
+
+    // Decrease life of live particles
+    ecs::make_system([&](ecs::entity_id ent, life& l, dead*) {
+        l.val -= delta_time;
+        if (l.val < 0) {
+            ecs::add_component(ent, dead{});
+            deads++;
+        }
+    });
+}
+
 void particles(benchmark::State& state) {
 	for ([[maybe_unused]] auto const _ : state) {
 		ecs::detail::_context.reset();
 
-		// Apply gravity to the velocity
-		ecs::make_parallel_system([](velocity &vel, gravity const& grav) {
-			vel.y -= grav.g * delta_time;
-		});
+        //state.PauseTiming();
+		    make_systems();
+		    ecs::add_component({0, max_num_particles},
+			    particle_init,
+			    velocity_init,
+			    color_init,
+			    life_init);
+		    ecs::commit_changes();
+        //state.ResumeTiming();
 
-		// Update a particles position from its velocity
-		ecs::make_parallel_system([](particle &par, velocity const& vel) {
-			par.x += vel.x * delta_time;
-			par.y += vel.y * delta_time;
-		});
-
-		// Make sure the particles stay within the bounds.
-		ecs::make_parallel_system([](particle& par, velocity& vel) {
-			if (par.x > 1) {
-				par.x = 1;
-				float const p = 2 * vel.x * -1;
-				vel.x = vel.x - p * -1;
-			}
-			else if (par.x < -1) {
-				par.x = -1;
-				float const p = 2 * vel.x * 1;
-				vel.x = vel.x - p * 1;
-			}
-
-			if (par.y > 1) {
-				par.y = 1;
-				float const p = 2 * vel.y * -1;
-				vel.y = vel.y - p * -1;
-			}
-			else if (par.y < -1) {
-				par.y = -1;
-				float const p = 2 * vel.y * 1;
-				vel.y = vel.y - p * 1;
-			}
-		});
-
-		// Paint particles purple if they are in range of (0.0, 0.0)
-		ecs::make_parallel_system([](color &col, particle const& par) {
-			float const len_sqr = par.x * par.x + par.y * par.y;
-
-			if (len_sqr > 0.0005f)
-				return; // out of range
-
-			col.r = 1;
-			col.g = 0;
-			col.b = 1;
-		});
-
-		ecs::add_components({0, max_num_particles},
-			particle_init,
-			velocity_init,
-			color_init,
-			life_init);
-		ecs::commit_changes();
-
-		for (int i = 0; i < num_frames; i++) {
-			ecs::run_systems();
+        for (int i = 0; i < num_frames; i++) {
+			ecs::update();
 		}
 	}
 }
