3e9cf41cd2d7c81c7a107b83eecf21f8c3f16df4
Kenneth Gorking <dasapfe@yahoo.dk>
2020-07-26 22:11:15 +0200

    Added empty/range tests to intersection test; made variables const

--- unittest/entity_range.cpp
+++ unittest/entity_range.cpp
@@ -13,14 +13,37 @@ TEST_CASE("entity_range ", "[entity]") {
             counter++;
         REQUIRE(counter == 2);
     }
+
     SECTION("intersection tests") {
+        SECTION("Empty range A") {
+            /// a:
+            /// b:      ---     ---     ---
+            std::vector<ecs::entity_range> const vec_a{};
+            std::vector<ecs::entity_range> const vec_b{{5, 7}, {13, 15}, {21, 23}};
+
+            auto const result = ecs::intersect_ranges(vec_a, vec_b);
+
+            CHECK(result.empty());
+        }
+
+        SECTION("Empty range B") {
+            /// a: *****   *****   *****
+            /// b:
+            std::vector<ecs::entity_range> const vec_a{{0, 4}, {8, 12}, {16, 20}};
+            std::vector<ecs::entity_range> const vec_b{};
+
+            auto const result = ecs::intersect_ranges(vec_a, vec_b);
+
+            CHECK(result.empty());
+        }
+
         SECTION("No overlaps between ranges") {
             /// a: *****   *****   *****
             /// b:      ---     ---     ---
-            std::vector<ecs::entity_range> vec_a{{0, 4}, {8, 12}, {16, 20}};
-            std::vector<ecs::entity_range> vec_b{{5, 7}, {13, 15}, {21, 23}};
+            std::vector<ecs::entity_range> const vec_a{{0, 4}, {8, 12}, {16, 20}};
+            std::vector<ecs::entity_range> const vec_b{{5, 7}, {13, 15}, {21, 23}};
 
-            auto result = ecs::intersect_ranges(vec_a, vec_b);
+            auto const result = ecs::intersect_ranges(vec_a, vec_b);
 
             REQUIRE(result.empty());
         }
@@ -28,10 +51,10 @@ TEST_CASE("entity_range ", "[entity]") {
         SECTION("Ranges in B are contained in ranges in A") {
             /// a: ***** ***** *****
             /// b:  ---   ---   ---
-            std::vector<ecs::entity_range> vec_a{{0, 4}, {5, 9}, {10, 14}};
-            std::vector<ecs::entity_range> vec_b{{1, 3}, {6, 8}, {11, 13}};
+            std::vector<ecs::entity_range> const vec_a{{0, 4}, {5, 9}, {10, 14}};
+            std::vector<ecs::entity_range> const vec_b{{1, 3}, {6, 8}, {11, 13}};
 
-            auto result = ecs::intersect_ranges(vec_a, vec_b);
+            auto const result = ecs::intersect_ranges(vec_a, vec_b);
 
             REQUIRE(3 == result.size());
             CHECK(ecs::entity_range{1, 3}.equals(result.at(0)));
@@ -42,10 +65,10 @@ TEST_CASE("entity_range ", "[entity]") {
         SECTION("Ranges in A are contained in ranges in B") {
             /// a:  ---   ---   ---
             /// b: ***** ***** *****
-            std::vector<ecs::entity_range> vec_a{{1, 3}, {6, 8}, {11, 13}};
-            std::vector<ecs::entity_range> vec_b{{0, 4}, {5, 9}, {10, 14}};
+            std::vector<ecs::entity_range> const vec_a{{1, 3}, {6, 8}, {11, 13}};
+            std::vector<ecs::entity_range> const vec_b{{0, 4}, {5, 9}, {10, 14}};
 
-            auto result = ecs::intersect_ranges(vec_a, vec_b);
+            auto const result = ecs::intersect_ranges(vec_a, vec_b);
 
             REQUIRE(3 == result.size());
             CHECK(ecs::entity_range{1, 3}.equals(result.at(0)));
@@ -56,10 +79,10 @@ TEST_CASE("entity_range ", "[entity]") {
         SECTION("Ranges in A overlap ranges in B") {
             /// a: *****  *****  *****
             /// b:     ---    ---    ---
-            std::vector<ecs::entity_range> vec_a{{0, 4}, {7, 11}, {14, 18}};
-            std::vector<ecs::entity_range> vec_b{{4, 6}, {11, 13}, {18, 20}};
+            std::vector<ecs::entity_range> const vec_a{{0, 4}, {7, 11}, {14, 18}};
+            std::vector<ecs::entity_range> const vec_b{{4, 6}, {11, 13}, {18, 20}};
 
-            auto result = ecs::intersect_ranges(vec_a, vec_b);
+            auto const result = ecs::intersect_ranges(vec_a, vec_b);
 
             REQUIRE(3 == result.size());
             CHECK(ecs::entity_range{4, 4} == result.at(0));
@@ -70,10 +93,10 @@ TEST_CASE("entity_range ", "[entity]") {
         SECTION("Ranges in B overlap ranges in A") {
             /// a:     ---    ---    ---
             /// b: *****  *****  *****
-            std::vector<ecs::entity_range> vec_a{{4, 6}, {11, 13}, {18, 20}};
-            std::vector<ecs::entity_range> vec_b{{0, 4}, {7, 11}, {14, 18}};
+            std::vector<ecs::entity_range> const vec_a{{4, 6}, {11, 13}, {18, 20}};
+            std::vector<ecs::entity_range> const vec_b{{0, 4}, {7, 11}, {14, 18}};
 
-            auto result = ecs::intersect_ranges(vec_a, vec_b);
+            auto const result = ecs::intersect_ranges(vec_a, vec_b);
 
             REQUIRE(3 == result.size());
             CHECK(ecs::entity_range{4, 4} == result.at(0));
@@ -84,10 +107,10 @@ TEST_CASE("entity_range ", "[entity]") {
         SECTION("Ranges in A overlap multiple ranges in B") {
             /// a: ********* *********
             /// b:  --- ---   --- ---
-            std::vector<ecs::entity_range> vec_a{{0, 8}, {9, 17}};
-            std::vector<ecs::entity_range> vec_b{{1, 3}, {5, 7}, {10, 12}, {14, 16}};
+            std::vector<ecs::entity_range> const vec_a{{0, 8}, {9, 17}};
+            std::vector<ecs::entity_range> const vec_b{{1, 3}, {5, 7}, {10, 12}, {14, 16}};
 
-            auto result = ecs::intersect_ranges(vec_a, vec_b);
+            auto const result = ecs::intersect_ranges(vec_a, vec_b);
 
             REQUIRE(4 == result.size());
             CHECK(ecs::entity_range{1, 3} == result.at(0));
@@ -99,10 +122,10 @@ TEST_CASE("entity_range ", "[entity]") {
         SECTION("Ranges in B overlap multiple ranges in A") {
             /// a:  --- ---   --- ---
             /// b: ********* *********
-            std::vector<ecs::entity_range> vec_a{{1, 3}, {5, 7}, {10, 12}, {14, 16}};
-            std::vector<ecs::entity_range> vec_b{{0, 8}, {9, 17}};
+            std::vector<ecs::entity_range> const vec_a{{1, 3}, {5, 7}, {10, 12}, {14, 16}};
+            std::vector<ecs::entity_range> const vec_b{{0, 8}, {9, 17}};
 
-            auto result = ecs::intersect_ranges(vec_a, vec_b);
+            auto const result = ecs::intersect_ranges(vec_a, vec_b);
 
             REQUIRE(4 == result.size());
             CHECK(ecs::entity_range{1, 3} == result.at(0));
@@ -114,10 +137,10 @@ TEST_CASE("entity_range ", "[entity]") {
         SECTION("One range in B overlaps two ranges in A") {
             /// a: *** ***
             /// b:  -----
-            std::vector<ecs::entity_range> vec_a{{1, 3}, {5, 7}};
-            std::vector<ecs::entity_range> vec_b{{2, 6}};
+            std::vector<ecs::entity_range> const vec_a{{1, 3}, {5, 7}};
+            std::vector<ecs::entity_range> const vec_b{{2, 6}};
 
-            auto result = ecs::intersect_ranges(vec_a, vec_b);
+            auto const result = ecs::intersect_ranges(vec_a, vec_b);
 
             REQUIRE(2 == result.size());
             CHECK(ecs::entity_range{2, 3} == result.at(0));
@@ -127,10 +150,10 @@ TEST_CASE("entity_range ", "[entity]") {
         SECTION("One range in A overlaps two ranges in B") {
             /// a:  -----
             /// b: *** ***
-            std::vector<ecs::entity_range> vec_a{{2, 6}};
-            std::vector<ecs::entity_range> vec_b{{1, 3}, {5, 7}};
+            std::vector<ecs::entity_range> const vec_a{{2, 6}};
+            std::vector<ecs::entity_range> const vec_b{{1, 3}, {5, 7}};
 
-            auto result = ecs::intersect_ranges(vec_a, vec_b);
+            auto const result = ecs::intersect_ranges(vec_a, vec_b);
 
             REQUIRE(2 == result.size());
             CHECK(ecs::entity_range{2, 3} == result.at(0));
@@ -278,7 +301,7 @@ TEST_CASE("entity_range ", "[entity]") {
         }
     }
 
-    SECTION("intersection merging") {
+    SECTION("merging ranges") {
         auto constexpr tester = [](std::vector<ecs::entity_range> input, std::vector<ecs::entity_range> const expected) {
             auto constexpr merger = [](ecs::entity_range& a, ecs::entity_range const& b) {
                 if (a.can_merge(b)) {
