1020a36580f9e5505787266874ecbb71ac0ea13a
Kenneth Gorking <dasapfe@yahoo.dk>
2020-07-30 13:19:18 +0200

    Made the systems serial so they won't print on top of each other

--- examples/sorting/sorting.cpp
+++ examples/sorting/sorting.cpp
@@ -17,19 +17,23 @@ auto constexpr sort_even_odd = [](int const& l, int const& r) {
 };
 
 int main() {
-    auto& sys_no_sort = ecs::make_system(printer);
-    auto& sys_sort_asc = ecs::make_system(printer, sort_asc);
-    auto& sys_sort_des = ecs::make_system(printer, sort_des);
-    auto& sys_sort_eo = ecs::make_system(printer, sort_even_odd);
+    auto& sys_no_sort = ecs::make_system<ecs::opts::not_parallel>(printer);
+    auto& sys_sort_asc = ecs::make_system<ecs::opts::not_parallel>(printer, sort_asc);
+    auto& sys_sort_des = ecs::make_system<ecs::opts::not_parallel>(printer, sort_des);
+    auto& sys_sort_eo = ecs::make_system<ecs::opts::not_parallel>(printer, sort_even_odd);
 
     ecs::add_component({0, 9}, generator);
     ecs::commit_changes();
 
     sys_no_sort.run();
     std::cout << '\n';
+
     sys_sort_asc.run();
     std::cout << '\n';
+
     sys_sort_des.run();
     std::cout << '\n';
+
     sys_sort_eo.run();
+    std::cout << '\n';
 }
