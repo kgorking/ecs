d17e335408423a98fcd5fc0e9fd8d6c0ee3daf90
Kenneth Gorking <dasapfe@yahoo.dk>
2020-08-05 00:21:13 +0200

    barf

--- include/ecs/detail/component_pool.h
+++ include/ecs/detail/component_pool.h
@@ -334,8 +334,13 @@ namespace ecs::detail {
                     auto a_rng = std::get<0>(a);
                     auto const b_rng = std::get<0>(b);
 
+                    #ifdef __clang__
                     auto a_func = std::get<1>(a);
                     auto b_func = std::get<1>(b);
+                    #else
+                    auto const a_func = std::get<1>(a);
+                    auto const b_func = std::get<1>(b);
+                    #endif
 
                     if (a_rng.can_merge(b_rng) && (a_func.template target<T(entity_id)>() ==  b_func.template target<T(entity_id)>())) {
                         a_rng = entity_range::merge(a_rng, b_rng);
