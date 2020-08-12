3277c7b4d03d64399cda8151e57f1e06f30aa0c7
Kenneth Gorking <dasapfe@yahoo.dk>
2020-08-12 18:37:38 +0200

    Fixed the contracts macros so breakpoints show on the same line as the contract.

--- include/ecs/detail/contract.h
+++ include/ecs/detail/contract.h
@@ -2,7 +2,7 @@
 #define __CONTRACT
 
 // Contracts. If they are violated, the program is an invalid state, so nuke it from orbit
-#define Expects(cond) ((cond) ? static_cast<void>(0) : std::terminate())
-#define Ensures(cond) ((cond) ? static_cast<void>(0) : std::terminate())
+#define Expects(cond) do { ((cond) ? static_cast<void>(0) : std::terminate()); } while(false)
+#define Ensures(cond) do { ((cond) ? static_cast<void>(0) : std::terminate()); } while(false)
 
 #endif // !__CONTRACT
