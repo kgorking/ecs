a4d7a0fff3fcc495ce76c80347cf3cb81085f1fa
Kenneth Gorking <kgorking@outlook.com>
2020-11-04 16:16:58 +0100

    Systems with frequency limiters can now run in parallel
    ...since the shared state bug was fixed.

--- examples/frequency/frequency.cpp
+++ examples/frequency/frequency.cpp
@@ -11,15 +11,15 @@ int main() {
     int counters[num_frequencies];
     std::fill_n(counters, num_frequencies, 0);
 
-    ecs::make_system<ecs::opts::frequency<frequencies[0]>, ecs::opts::not_parallel>([&counters](int const&) { ++counters[0]; });
-    ecs::make_system<ecs::opts::frequency<frequencies[1]>, ecs::opts::not_parallel>([&counters](int const&) { ++counters[1]; });
-    ecs::make_system<ecs::opts::frequency<frequencies[2]>, ecs::opts::not_parallel>([&counters](int const&) { ++counters[2]; });
-    ecs::make_system<ecs::opts::frequency<frequencies[3]>, ecs::opts::not_parallel>([&counters](int const&) { ++counters[3]; });
-    ecs::make_system<ecs::opts::frequency<frequencies[4]>, ecs::opts::not_parallel>([&counters](int const&) { ++counters[4]; });
-    ecs::make_system<ecs::opts::frequency<frequencies[5]>, ecs::opts::not_parallel>([&counters](int const&) { ++counters[5]; });
-    ecs::make_system<ecs::opts::frequency<frequencies[6]>, ecs::opts::not_parallel>([&counters](int const&) { ++counters[6]; });
-    ecs::make_system<ecs::opts::frequency<frequencies[7]>, ecs::opts::not_parallel>([&counters](int const&) { ++counters[7]; });
-    ecs::make_system<ecs::opts::frequency<frequencies[8]>, ecs::opts::not_parallel>([&counters](int const&) { ++counters[8]; });
+    ecs::make_system<ecs::opts::frequency<frequencies[0]>>([&counters](int const&) { ++counters[0]; });
+    ecs::make_system<ecs::opts::frequency<frequencies[1]>>([&counters](int const&) { ++counters[1]; });
+    ecs::make_system<ecs::opts::frequency<frequencies[2]>>([&counters](int const&) { ++counters[2]; });
+    ecs::make_system<ecs::opts::frequency<frequencies[3]>>([&counters](int const&) { ++counters[3]; });
+    ecs::make_system<ecs::opts::frequency<frequencies[4]>>([&counters](int const&) { ++counters[4]; });
+    ecs::make_system<ecs::opts::frequency<frequencies[5]>>([&counters](int const&) { ++counters[5]; });
+    ecs::make_system<ecs::opts::frequency<frequencies[6]>>([&counters](int const&) { ++counters[6]; });
+    ecs::make_system<ecs::opts::frequency<frequencies[7]>>([&counters](int const&) { ++counters[7]; });
+    ecs::make_system<ecs::opts::frequency<frequencies[8]>>([&counters](int const&) { ++counters[8]; });
 
     ecs::add_component({0, 0}, int{});
     ecs::commit_changes();
