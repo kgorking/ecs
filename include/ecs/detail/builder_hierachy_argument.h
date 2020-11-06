bf8c113e29172402ff19e299ca45631daed47475
Kenneth Gorking <kgorking@outlook.com>
2020-11-06 10:34:05 +0100

    Added the hierarchy builder class. Only does linear runs atm, but it's a start.

---
+++ include/ecs/detail/builder_hierachy_argument.h
@@ -0,0 +1,85 @@
+#ifndef __BUILDER_HIERARCHY_ARGUMENT_H_
+#define __BUILDER_HIERARCHY_ARGUMENT_H_
+
+// !Only to be included by system.h
+
+namespace ecs::detail {
+    template<typename Options, typename UpdateFn, typename SortFn, class FirstComponent, class... Components>
+    struct builder_hierarchy_argument {
+        // Determine the execution policy from the options (or lack thereof)
+        using execution_policy = std::conditional_t<ecs::detail::has_option<opts::not_parallel, Options>(),
+            std::execution::sequenced_policy, std::execution::parallel_policy>;
+
+        builder_hierarchy_argument(
+            UpdateFn update_func, SortFn /*sort*/, pool<FirstComponent> first_pool, pool<Components>... pools)
+            : pools{first_pool, pools...}
+            , update_func{update_func} {
+        }
+
+        builder_hierarchy_argument(UpdateFn update_func, SortFn /*sort*/, pool<Components>... pools)
+            : pools{pools...}
+            , update_func{update_func} {
+        }
+
+        tup_pools<FirstComponent, Components...> get_pools() const {
+            return pools;
+        }
+
+        void run() {
+            auto const e_p = execution_policy{}; // cannot pass 'execution_policy{}' directly to for_each in gcc
+            std::for_each(e_p, arguments.begin(), arguments.end(), [this](auto packed_arg) {
+                if constexpr (is_entity<FirstComponent>) {
+                    update_func(std::get<0>(packed_arg), extract_arg<Components>(packed_arg, 0)...);
+                } else {
+                    update_func(extract_arg<FirstComponent>(packed_arg, 0), extract_arg<Components>(packed_arg, 0)...);
+                }
+            });
+        }
+
+        // Convert a set of entities into arguments that can be passed to the system
+        void build(entity_range_view entities) {
+            if (entities.size() == 0) {
+                arguments.clear();
+                return;
+            }
+
+            // Count the total number of arguments
+            size_t arg_count = 0;
+            for (auto const& range : entities) {
+                arg_count += range.count();
+            }
+
+            // Reserve space for the arguments
+            arguments.clear();
+            arguments.reserve(arg_count);
+
+            // Build the arguments for the ranges
+            for (auto const& range : entities) {
+                for (entity_id const& entity : range) {
+                    if constexpr (is_entity<FirstComponent>) {
+                        arguments.emplace_back(entity, get_component<Components>(entity, pools)...);
+                    } else {
+                        arguments.emplace_back(entity, get_component<FirstComponent>(entity, pools),
+                            get_component<Components>(entity, pools)...);
+                    }
+                }
+            }
+        }
+
+    private:
+        // Holds a single entity id and its arguments
+        using single_argument =
+            decltype(std::tuple_cat(std::tuple<entity_id>{0}, argument_tuple<FirstComponent, Components...>{}));
+
+        // A tuple of the fully typed component pools used by this system
+        tup_pools<FirstComponent, Components...> const pools;
+
+        // The user supplied system
+        UpdateFn update_func;
+
+        // The vector of unrolled arguments, sorted using 'sort_func'
+        std::vector<single_argument> arguments;
+    };
+} // namespace ecs::detail
+
+#endif // !__BUILDER_HIERARCHY_ARGUMENT_H_
--- include/ecs/detail/builder_selector.h
+++ include/ecs/detail/builder_selector.h
@@ -3,6 +3,7 @@
 
 #include "builder_ranged_argument.h"
 #include "builder_sorted_argument.h"
+#include "builder_hierachy_argument.h"
 
 namespace ecs::detail {
 
@@ -10,9 +11,16 @@ namespace ecs::detail {
     template<typename Options, typename UpdateFn, typename SortFn, class FirstComponent, class... Components>
     constexpr auto get_ptr_builder() {
         bool constexpr has_sort_func = !std::is_same_v<SortFn, std::nullptr_t>;
+        bool constexpr has_parent = std::is_same_v<std::remove_cvref_t<FirstComponent>, ecs::parent> ||
+                                    (std::is_same_v<std::remove_cvref_t<Components>, ecs::parent> || ...);
+
+        static_assert(!(has_sort_func == has_parent && has_parent == true),
+            "Systems can not both be hierarchial and sorted");
 
         if constexpr (has_sort_func) {
             return (builder_sorted_argument<Options, UpdateFn, SortFn, FirstComponent, Components...>*) nullptr;
+        } else if constexpr (has_parent) {
+            return (builder_hierarchy_argument<Options, UpdateFn, SortFn, FirstComponent, Components...>*) nullptr;
         } else {
             return (builder_ranged_argument<Options, UpdateFn, SortFn, FirstComponent, Components...>*) nullptr;
         }
