18ec509ff5e14a902c8d3d9c40ea611ccf96dd06
Kenneth Gorking <kgorking@outlook.com>
2020-12-02 13:51:48 +0100

    Removed old code

--- include/ecs/detail/builder_selector.h
+++ include/ecs/detail/builder_selector.h
@@ -5,28 +5,4 @@
 #include "builder_sorted_argument.h"
 #include "builder_hierachy_argument.h"
 
-namespace ecs::detail {
-    // Chooses an argument builder and returns a nullptr to it
-    /*template<typename Options, typename UpdateFn, typename SortFn, class FirstComponent, class... Components>
-    constexpr auto get_ptr_builder() {
-        bool constexpr has_sort_func = !std::is_same_v<SortFn, std::nullptr_t>;
-        bool constexpr has_parent = is_parent<FirstComponent>::value || (is_parent<Components>::value || ...);
-
-        static_assert(!(has_sort_func == has_parent && has_parent == true),
-            "Systems can not both be hierarchial and sorted");
-
-        if constexpr (has_sort_func) {
-            return (builder_sorted_argument<Options, UpdateFn, SortFn, FirstComponent, Components...>*) nullptr;
-        } else if constexpr (has_parent) {
-            return (builder_hierarchy_argument<Options, UpdateFn, SortFn, FirstComponent, Components...>*) nullptr;
-        } else {
-            return (builder_ranged_argument<Options, UpdateFn, SortFn, FirstComponent, Components...>*) nullptr;
-        }
-    }
-
-    template<typename Options, typename UpdateFn, typename SortFn, class FirstComponent, class... Components>
-    using builder_selector =
-        std::remove_pointer_t<decltype(get_ptr_builder<Options, UpdateFn, SortFn, FirstComponent, Components...>())>;*/
-} // namespace ecs::detail
-
 #endif // !__BUILDER_SELECTOR_H
