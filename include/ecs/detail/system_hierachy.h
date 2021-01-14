1a025be3224f82f23e7cad3c952072b6d94f8765
Kenneth Gorking <kgorking@outlook.com>
2021-01-14 22:08:17 +0100

    Simplified and parallelized the construction of hierarchies.

--- include/ecs/detail/system_hierachy.h
+++ include/ecs/detail/system_hierachy.h
@@ -3,11 +3,12 @@
 
 #include <map>
 #include <unordered_map>
-#include <unordered_set>
+#include <future>
 
 #include "system.h"
 #include "find_entity_pool_intersections.h"
 #include "../parent.h"
+#include "entity_range_iterator.h"
 
 namespace ecs::detail {
     template<class Options, class UpdateFn, class TupPools, class FirstComponent, class... Components>
@@ -18,6 +19,10 @@ namespace ecs::detail {
 
         using relation_mmap = std::multimap<entity_type, entity_type>;
 
+        using entity_info = std::pair<int, entity_type>; // parent count, root id
+        using info_map = std::unordered_map<entity_type, entity_info>;
+        using info_iterator = info_map::iterator;
+
     public:
         system_hierarchy(UpdateFn update_func, TupPools pools)
             : system<Options, UpdateFn, TupPools, FirstComponent, Components...>{update_func, pools}
@@ -25,8 +30,8 @@ namespace ecs::detail {
                 std::apply([&pools](auto... parent_types) {
                     return std::make_tuple(&get_pool<decltype(parent_types)>(pools)...);
                 },
-                parent_types_tuple_t<parent_type>{})
-            } {
+                parent_types_tuple_t<parent_type>{})}
+            , pool_parent_id{get_pool<parent_id>(pools)} {
         }
 
     private:
@@ -49,93 +54,19 @@ namespace ecs::detail {
                 return;
             }
 
-            // Count the total number of arguments
-            size_t arg_count = 0;
-            for (auto const& range : ranges) {
-                arg_count += range.count();
-            }
-
-            // Clear the arguments and reserve space for new ones
+            // Clear the arguments
             arguments.clear();
-            arguments.reserve(arg_count);
-
-            // The component pool for the parent ids
-            component_pool<parent_id> const& pool_parent_id = get_pool<parent_id>(this->pools);
-
-            // • find a vertex with no incoming edges, put it in the output;
-            // • delete all its outgoing edges;
-            // • repeat.
-
-            // Find the roots; map parents to their children.
-            // 'ranges' holds all the entities that has a 'ecs::parent' on them, so any parent id I test
-            // against the pool will tell me if it is a root if it doesn't exist in the pool.
-            std::unordered_set<entity_type> roots;
-            relation_mmap parent_argument;
-
-            for (auto const& range : ranges) {
-                // Get the ranges span of the parent-id components
-                auto const span_parent_ids =
-                    std::span{pool_parent_id.find_component_data(range.first()), range.count()};
-
-                for (auto const id : range) {
-                    // Get the parent id
-                    auto const offset = id - range.first();
-                    entity_type const parent_id = span_parent_ids[offset];
-
-                    // Entities can't be parents of themselves
-                    Expects(id != parent_id);
-
-                    if (!pool_parent_id.has_entity(parent_id))
-                        roots.insert(parent_id);
-
-                    // Map the parent to its children
-                    parent_argument.emplace(parent_id, id);
-                }
-            }
-
-            // Do the depth-first search on the roots
-            std::vector<entity_type> rearranged_args;
-            rearranged_args.reserve(arg_count);
-            for (entity_type const& root : roots) {
-                depth_first_search(root, rearranged_args, parent_argument);
-            }
-
-            // This contract triggers on cyclical graphs, which are not supported
-            Expects(rearranged_args.size() == arg_count);
+            info.clear();
 
             // Build the arguments for the ranges
-            for (entity_id const entity : rearranged_args) {
-                // If the parent has sub-components specified, verify them
-                if constexpr (0 != std::tuple_size_v<decltype(parent_pools)>) {
-                    // Does tests on the parent sub-components to see they satisfy the constraints
-                    // ie. a 'parent<int*, float>' will return false if the parent does not have a float or
-                    // has an int.
-                    constexpr parent_types_tuple_t<parent_type> ptt{};
-                    bool const has_parent_types = std::apply(
-                        [&](auto... parent_types) {
-                            auto const check_parent = [&](auto parent_type) {
-                                // Get the parent components id
-                                parent_id const pid = *pool_parent_id.find_component_data(entity);
-
-                                // Get the pool of the parent sub-component
-                                auto const& sub_pool = get_pool<decltype(parent_type)>(this->pools);
-
-                                if constexpr (std::is_pointer_v<decltype(parent_type)>) {
-                                    // The type is a filter, so the parent is _not_ allowed to have this component
-                                    return !sub_pool.has_entity(pid);
-                                } else {
-                                    // The parent must have this component
-                                    return sub_pool.has_entity(pid);
-                                }
-                            };
-
-                            return (check_parent(parent_types) && ...);
-                        },
-                        ptt);
-
-                    if (!has_parent_types)
+            for (entity_id const entity : range_view_wrapper{ranges}) {
+                if constexpr (has_parent_types()) {
+                    if (!has_required_parent_types(entity)) {
                         continue;
+                    }
                 }
+
+                fill_entity_info(entity);
 
                 if constexpr (is_entity<FirstComponent>) {
                     arguments.emplace_back(entity, get_component<Components>(entity, this->pools)...);
@@ -144,24 +75,89 @@ namespace ecs::detail {
                         get_component<Components>(entity, this->pools)...);
                 }
             }
+
+            auto const topological_sort_func = [this](single_argument const& arg_l, single_argument const& arg_r) {
+                entity_id const id_l = std::get<0>(arg_l);
+                entity_id const id_r = std::get<0>(arg_r);
+
+                auto const [count_l, root_l] = info[id_l];
+                auto const [count_r, root_r] = info[id_r];
+
+                // order by roots
+                if (root_l != root_r)
+                    return root_l < root_r;
+
+                // order by depth
+                if (count_l != count_r)
+                    return count_l < count_r;
+
+                // order by id (irrelevant, but needed)
+                return id_l < id_r;
+            };
+
+            std::sort(std::execution::par, arguments.begin(), arguments.end(), topological_sort_func);
         }
 
-        void depth_first_search(entity_type ent, std::vector<entity_type>& vec, relation_mmap& parent_argument) {
-            // Get all the entities that has 'ent' as a parent
-            auto const [first, last] = parent_argument.equal_range(ent);
-            auto current = first;
+        entity_info fill_entity_info(entity_id const entity) {
+            // see if the entity exist in the map
+            auto const ent_it = info.find(entity);
+            if (ent_it != info.end())
+                return ent_it->second;
+            
+            // Get the parent id
+            entity_id const* parent_id = pool_parent_id.find_component_data(entity);
+            if (parent_id == nullptr) {
+                // This entity does not have a 'parent_id' component,
+                // which means that this entity is a root
+                auto const [it, _] = info.emplace(std::make_pair(entity, entity_info{0, entity}));
+                return it->second;
+            }
 
-            while (current != last) {
-                if (current->second != ent) {
-                    entity_type const id = current->second;
-                    current->second = ent; // mark the node as visited (points to itself)
+            // look up the parent info
+            auto const [parent_count, parent_root] = fill_entity_info(*parent_id);
 
-                    vec.push_back(id);
+            // insert the entity info
+            auto const [it, $] =
+                info.emplace(std::make_pair(entity, entity_info{1 + parent_count, parent_root}));
+            return it->second;
+        }
 
-                    depth_first_search(id, vec, parent_argument);
-                }
+        constexpr static bool has_parent_types() {
+            return (0 != std::tuple_size_v<decltype(parent_pools)>);
+        }
 
-                ++current;
+        bool has_required_parent_types(entity_id const entity) const {
+            // If the parent has sub-components specified, verify them
+            if constexpr (0 != std::tuple_size_v<decltype(parent_pools)>) {
+                // Does tests on the parent sub-components to see they satisfy the constraints
+                // ie. a 'parent<int*, float>' will return false if the parent does not have a float or
+                // has an int.
+                constexpr parent_types_tuple_t<parent_type> ptt{};
+                bool const has_parent_types = std::apply(
+                    [&](auto... parent_types) {
+                        auto const check_parent = [&](auto parent_type) {
+                            // Get the parent components id
+                            parent_id const pid = *pool_parent_id.find_component_data(entity);
+
+                            // Get the pool of the parent sub-component
+                            auto const& sub_pool = get_pool<decltype(parent_type)>(this->pools);
+
+                            if constexpr (std::is_pointer_v<decltype(parent_type)>) {
+                                // The type is a filter, so the parent is _not_ allowed to have this component
+                                return !sub_pool.has_entity(pid);
+                            } else {
+                                // The parent must have this component
+                                return sub_pool.has_entity(pid);
+                            }
+                        };
+
+                        return (check_parent(parent_types) && ...);
+                    },
+                    ptt);
+
+                return has_parent_types;
+            } else {
+                return true;
             }
         }
 
@@ -179,6 +175,12 @@ namespace ecs::detail {
 
         // The vector of unrolled arguments
         std::vector<single_argument> arguments;
+
+        // map of entity info
+        info_map info;
+
+        // The pool that holds 'parent_id's
+        component_pool<parent_id> const& pool_parent_id;
 
         // A tuple of the fully typed component pools used the parent component
         parent_pool_tuple_t<parent_type> const parent_pools;
