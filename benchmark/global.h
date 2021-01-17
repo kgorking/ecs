c17d3c5b2bae7dad910eb78e2dc2811a3788920d
Kenneth Gorking <kgorking@outlook.com>
2021-01-17 20:58:54 +0100

    Increased the number of entities to benchmark

--- benchmark/global.h
+++ benchmark/global.h
@@ -3,10 +3,10 @@
 #include <ecs/flags.h>
 #include <ecs/entity_id.h>
 
-size_t constexpr start_components = 256;
+size_t constexpr start_components = 512;
 // size_t constexpr num_components = 256 * 1024;
-//size_t constexpr num_components = start_components * start_components;
-size_t constexpr num_components = 1024 * 1024;
+size_t constexpr num_components = start_components * start_components;
+//size_t constexpr num_components = 256 * 256;
 
 struct global_s {
 	ecs_flags(ecs::flag::global);
