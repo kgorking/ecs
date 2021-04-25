06995bffa6064ca5f95db0f77d1c7e2203e207f6
Kenneth Gorking <kgorking@outlook.com>
2021-04-25 13:02:46 +0200

    Simplified the basic example, the old one was overly complicated

--- examples/example/example.cpp
+++ examples/example/example.cpp
@@ -1,53 +1,20 @@
-#include <ecs/ecs.h>
 #include <iostream>
-#include <string>
+#include <ecs/ecs.h>
 
-// Systems are placed in different groups so they
-// don't write text at the same time to the console.
+// The component
+struct greeting {
+    char const* msg;
+};
 
 int main() {
-    using serial = ecs::opts::not_parallel;
-
-    std::cout << "# 1. A simple example\n";
-    // A system that operates on entities that hold an 'int'
-    ecs::make_system<serial>([](int const& i) { std::cout << i << '\n'; });
-
-    // Set up 3 entities with their components
-    // This uses the entity_range class, which is a
-    // wrapper for the interface to allow easy usage
-    ecs::add_component({0, 2}, int{1});
-
-    // Commit the changes and run the systems
-    ecs::update();
-
-    std::cout << "\n# 2. using a lambda to initialize components\n";
-    ecs::add_component({3, 5}, [](ecs::entity_id ent) -> int { return ent * 2; });
-    ecs::update();
-
-    std::cout << "\n# 3. Adding a second component\n";
-    // Add another system that operates on entities that hold an 'int' and 'std::string'
-    ecs::make_system<serial>(
-        [](int const& i, std::string const& s) { std::cout << i << ": " << s << '\n'; });
-
-    // Add a second component to the last 3 entities
-    ecs::add_component(3, std::string{"jon"});
-    ecs::add_component(4, std::string{"sean"});
-    ecs::add_component(5, std::string{"jimmy"});
-
-    // Commit the changes and run the systems
-    ecs::update();
-
-    std::cout << "\n# 4. Removing a component\n";
-    // Remove the integer component from the 'sean' entity using the 'ecc::entity' helper class
-    ecs::entity_id const sean{4};
-    ecs::remove_component<int>(sean);
-
-    // Commit the changes and run the systems
-    ecs::update();
-
-    std::cout << "\n# 5. Accessing the entity id\n";
-    ecs::make_system<serial>([](ecs::entity_id ent, std::string const& s) {
-        std::cout << "entity with id " << ent << " is named " << s << '\n';
+    // The system
+    ecs::make_system([](greeting const& g) {
+        std::cout << g.msg;
     });
+
+    // The entities
+    ecs::add_component({0, 2}, greeting{"alright "});
+
+    // Run the system on all entities with a 'greeting' component
     ecs::update();
 }
