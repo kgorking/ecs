d39433b83871cc5c47f6c3fea7ef9e02ee02bc48
Kenneth Gorking <kgorking@outlook.com>
2021-10-20 18:35:54 +0200

    Fixed up some implicit conversions

--- benchmark/bench_baseline.cpp
+++ benchmark/bench_baseline.cpp
@@ -7,31 +7,36 @@
 
 template <bool parallel>
 void run_raw(benchmark::State &state) {
-	auto const nentities = static_cast<ecs::detail::entity_type>(state.range(0));
+	auto const num_items = state.range(0);
+	auto const num_entities = static_cast<ecs::detail::entity_type>(num_items);
+	auto const num_colors = static_cast<size_t>(num_items) + 1;
 
-	std::vector<int> colors(nentities + 1);
-	std::fill_n(colors.begin(), nentities, int{});
+	std::vector<int> colors(num_colors);
+	std::fill_n(colors.begin(), num_colors, int{});
 
-	ecs::entity_range const range{0, nentities};
+	ecs::entity_range const range{0, num_entities};
 
 	auto constexpr e_p = std::conditional_t<parallel, std::execution::parallel_policy, std::execution::sequenced_policy>{};
 	for ([[maybe_unused]] auto const _ : state) {
 		std::for_each(e_p, range.begin(), range.end(), [&](ecs::entity_id ent) { benchmark_system(ent, colors[ent]); });
 	}
 
-	state.SetItemsProcessed(state.iterations() * nentities);
+	state.SetItemsProcessed(state.iterations() * num_items);
 }
 
 // Simulates the hierarchial setup from system_hiearchy
 template <bool parallel>
 void run_raw_hierarchy(benchmark::State &state) {
-	auto const nentities = static_cast<size_t>(state.range(0));
+	auto const num_items = state.range(0);
+	auto const num_entities = static_cast<ecs::detail::entity_type>(num_items);
+	auto const num_colors = static_cast<size_t>(num_items) + 1;
+
 
 	//
 	// Simulate the central storage.
 
-	std::vector<int> colors(nentities + 1);
-	std::fill_n(colors.begin(), nentities + 1, 0);
+	std::vector<int> colors(num_colors);
+	std::fill_n(colors.begin(), num_colors, 0);
 
 	//
 	// Simulate a work unit that operates on 'colors'.
@@ -41,7 +46,7 @@ void run_raw_hierarchy(benchmark::State &state) {
 	// Create the 'colors' arguments.
 	// Holds an id and a pointer to that id's color.
 	std::vector<argument> args;
-	std::generate_n(std::back_inserter(args), nentities + 1, [i = 0, &colors]() mutable {
+	std::generate_n(std::back_inserter(args), num_colors, [i = 0, &colors]() mutable {
 		auto a = argument(ecs::entity_id{i}, colors.data() + i);
 		i++;
 		return a;
@@ -55,12 +60,12 @@ void run_raw_hierarchy(benchmark::State &state) {
 	const size_t span_size = 8;
 	std::vector<std::span<argument>> argument_spans;
 	size_t count = 0;
-	while (count + span_size < nentities) {
+	while (count + span_size < num_entities) {
 		auto const cluster = span_all_args.subspan(count+1, span_size-1);
 		argument_spans.push_back(cluster);
 		count += span_size;
 	}
-	argument_spans.push_back(span_all_args.subspan(count, nentities - count));
+	argument_spans.push_back(span_all_args.subspan(count, num_entities - count));
 
 	// Determine the execution policy to use
 	auto constexpr e_p = std::conditional_t<parallel, std::execution::parallel_policy, std::execution::sequenced_policy>{};
@@ -75,7 +80,7 @@ void run_raw_hierarchy(benchmark::State &state) {
 		});
 	}
 
-	state.SetItemsProcessed(state.iterations() * nentities);
+	state.SetItemsProcessed(state.iterations() * num_items);
 }
 
 void run_raw_serial(benchmark::State &state) {
