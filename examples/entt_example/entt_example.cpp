0bd8c96876a964e7a9f21bc636f20deeeff5b7d8
Kenneth Gorking <kgorking@outlook.com>
2021-12-29 16:07:27 +0100

    Commented out unused params

--- examples/entt_example/entt_example.cpp
+++ examples/entt_example/entt_example.cpp
@@ -1,8 +1,5 @@
 #include <ecs/ecs.h>
 
-// disable 'unreferenced formal parameter' warnings
-#pragma warning(disable : 4100)
-
 // An example structured like the EnTT example from
 // https://github.com/skypjack/entt#code-example
 // to compare interfaces
@@ -20,8 +17,8 @@ struct velocity {
 int main() {
 	ecs::runtime ecs;
 
-	ecs.make_system([](ecs::entity_id id, position& pos, velocity const& vel) { /* ... */ });
-	ecs.make_system([](velocity& vel) { /* ... */ });
+	ecs.make_system([](ecs::entity_id /*id*/, position& /*pos*/, velocity const& /*vel*/) { /* ... */ });
+	ecs.make_system([](velocity& /*vel*/) { /* ... */ });
 
 	ecs.add_component({0, 9}, position{}, velocity{});
 
