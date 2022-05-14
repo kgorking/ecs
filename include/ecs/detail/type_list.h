8037f3dcf5d490849184810077de76269d7cac3d
Kenneth Gorking <kgorking@outlook.com>
2022-05-14 16:51:12 +0200

    Removed type_list_at, no longer used

--- include/ecs/detail/type_list.h
+++ include/ecs/detail/type_list.h
@@ -30,45 +30,11 @@ namespace impl {
 		static constexpr size_t value = sizeof...(Types);
 	};
 
-	//
-	// type_list_at.
-	template <int, typename>
-	struct type_list_at;
-	template <int I, typename Type, typename... Types>
-	struct type_list_at<I, type_list<Type, Types...>> {
-		using type = typename type_list_at<I - 1, type_list<Types...>>::type;
-	};
-	template <typename Type, typename... Types>
-	struct type_list_at<0, type_list<Type, Types...>> {
-		using type = Type;
-	};
 
 	//
-	// type_list_at_or.
-	template <int, typename OrType, typename TypeList>
-	struct type_list_at_or;
-
-	template <int I, typename OrType, typename Type, typename... Types>
-	struct type_list_at_or<I, OrType, type_list<Type, Types...>> {
-		using type = typename type_list_at_or<I - 1, OrType, type_list<Types...>>::type;
-	};
-
-	template <typename Type, typename OrType, typename... Types>
-	struct type_list_at_or<0, OrType, type_list<Type, Types...>> {
-		using type = Type;
-	};
-
-	template <typename Type, typename OrType, typename... Types>
-	struct type_list_at_or<int{-1}, OrType, type_list<Type, Types...>> {
-		using type = OrType;
-	};
-
-
-	//
-	// type_list concepts
+	// type_list concept
 	template <class TL>
 	concept TypeList = detect_type_list(static_cast<TL*>(nullptr));
-
 
 
 	//
@@ -122,12 +88,6 @@ namespace impl {
 
 template <impl::TypeList TL>
 constexpr size_t type_list_size = impl::type_list_size<TL>::value;
-
-template <int I, impl::TypeList TL>
-using type_list_at = typename impl::type_list_at<I, TL>::type;
-
-template <int I, impl::TypeList TL, typename OrType>
-using type_list_at_or = typename impl::type_list_at_or<I, OrType, TL>::type;
 
 // Applies the functor F to each type in the type list.
 // Takes lambdas of the form '[]<typename T>() {}'
