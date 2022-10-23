674e7f45d97ec9ad5ac386018c633b45006f5ec2
Kenneth Gorking <kgorking@outlook.com>
2022-10-23 21:06:10 +0200

    Added a missing constructor to parent_id

--- include/ecs/detail/parent_id.h
+++ include/ecs/detail/parent_id.h
@@ -6,7 +6,9 @@
 namespace ecs::detail {
 
 // The parent type stored internally in component pools
-struct parent_id : entity_id {};
+struct parent_id : entity_id {
+	constexpr parent_id(detail::entity_type _id) noexcept : entity_id(_id) {}
+};
 
 } // namespace ecs::detail
 
