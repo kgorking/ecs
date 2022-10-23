2b2131ae9137ce264fd81076e5129ce7fd95abb5
Kenneth Gorking <kgorking@outlook.com>
2022-10-23 21:07:52 +0200

    Use vector::push_back so clang can compile

--- include/ecs/detail/system_sorted.h
+++ include/ecs/detail/system_sorted.h
@@ -46,11 +46,11 @@ private:
 
 		apply_type<ComponentsList>([&]<typename... Types>() {
 			find_entity_pool_intersections_cb<ComponentsList>(this->pools, [this, index = 0u](entity_range range) mutable {
-				lambda_arguments.emplace_back(make_argument<Types...>(range, get_component<Types>(range.first(), this->pools)...));
+				lambda_arguments.push_back(make_argument<Types...>(range, get_component<Types>(range.first(), this->pools)...));
 
 				for (entity_id const entity : range) {
 					entity_offset const offset = range.offset(entity);
-					sorted_args.emplace_back(index, offset, get_component<sort_types>(entity, this->pools));
+					sorted_args.push_back({index, offset, get_component<sort_types>(entity, this->pools)});
 				}
 
 				index += 1;
