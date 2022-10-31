bf7882a2224894058db330f7b20220cab0cded8c
Kenneth Gorking <kgorking@outlook.com>
2022-10-31 12:10:46 +0100

    First commit

---
+++ include/ecs/detail/static_scheduler.h
@@ -0,0 +1,10 @@
+#ifndef ECS_DETAIL_STATIC_SCHEDULER_H
+#define ECS_DETAIL_STATIC_SCHEDULER_H
+
+//
+// Stuff to do before work on the scheduler can proceed:
+//   * unify the argument creation across systems
+//   * ? disallow captures in user supplied system lambdas
+//
+
+#endif // !ECS_DETAIL_STATIC_SCHEDULER_H
