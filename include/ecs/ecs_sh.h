7b5551926365e0b52fcd20da712eb7ada45490be
Kenneth Gorking <kgorking@outlook.com>
2023-01-27 14:19:03 +0100

    Fixed wronlgy placed include

--- include/ecs/ecs_sh.h
+++ include/ecs/ecs_sh.h
@@ -1443,6 +1443,45 @@ struct global {};
 } // namespace ecs::flag
 
 #endif // !ECS_FLAGS_H
+#ifndef ECS_DETAIL_STRIDE_VIEW_H
+#define ECS_DETAIL_STRIDE_VIEW_H
+
+
+namespace ecs::detail {
+
+// 
+template <std::size_t Stride, typename T>
+class stride_view {
+	char const* first{nullptr};
+	char const* curr{nullptr};
+	char const* last{nullptr};
+
+public:
+	stride_view() noexcept = default;
+	stride_view(T const* first_, std::size_t count_) noexcept
+		: first{reinterpret_cast<char const*>(first_)}
+		, curr {reinterpret_cast<char const*>(first_)}
+		, last {reinterpret_cast<char const*>(first_) + Stride*count_} {
+		Expects(first_ != nullptr);
+	}
+
+	T const* current() const noexcept {
+		return reinterpret_cast<T const*>(curr);
+	}
+
+	bool done() const noexcept {
+		return (first==nullptr) || (curr >= last);
+	}
+
+	void next() noexcept {
+		if (!done())
+			curr += Stride;
+	}
+};
+
+}
+
+#endif //!ECS_DETAIL_STRIDE_VIEW_H
 #ifndef ECS_DETAIL_FLAGS_H
 #define ECS_DETAIL_FLAGS_H
 
@@ -3046,45 +3085,6 @@ inline std::vector<entity_range> difference_ranges(entity_range_view view_a, ent
 } // namespace ecs::detail
 
 #endif // !ECS_DETAIL_ENTITY_RANGE
-#ifndef ECS_DETAIL_STRIDE_VIEW_H
-#define ECS_DETAIL_STRIDE_VIEW_H
-
-
-namespace ecs::detail {
-
-// 
-template <std::size_t Stride, typename T>
-class stride_view {
-	char const* first{nullptr};
-	char const* curr{nullptr};
-	char const* last{nullptr};
-
-public:
-	stride_view() noexcept = default;
-	stride_view(T const* first_, std::size_t count_) noexcept
-		: first{reinterpret_cast<char const*>(first_)}
-		, curr {reinterpret_cast<char const*>(first_)}
-		, last {reinterpret_cast<char const*>(first_) + Stride*count_} {
-		Expects(first_ != nullptr);
-	}
-
-	T const* current() const noexcept {
-		return reinterpret_cast<T const*>(curr);
-	}
-
-	bool done() const noexcept {
-		return (first==nullptr) || (curr >= last);
-	}
-
-	void next() noexcept {
-		if (!done())
-			curr += Stride;
-	}
-};
-
-}
-
-#endif //!ECS_DETAIL_STRIDE_VIEW_H
 #ifndef ECS_FIND_ENTITY_POOL_INTERSECTIONS_H
 #define ECS_FIND_ENTITY_POOL_INTERSECTIONS_H
 
