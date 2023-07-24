72b8390edc38b0d35032b8a5f933477333496521
Kenneth Gorking <kgorking@outlook.com>
2023-07-24 14:19:34 +0200

    Removed code

--- include/ecs/detail/component_pools.h
+++ include/ecs/detail/component_pools.h
@@ -10,25 +10,18 @@ class component_pool_base;
 template <typename, typename> class component_pool;
 
 template<typename T>
-using modstrip = std::remove_pointer_t<std::remove_cvref_t<reduce_parent_t<T>>>;
+using modstrip_t = std::remove_pointer_t<std::remove_cvref_t<reduce_parent_t<T>>>;
 
 // 
 template <typename ComponentsList>
-struct component_pools : type_list_indices<transform_type<ComponentsList, modstrip>> {
-	constexpr component_pools() noexcept = default;
-	constexpr component_pools(component_pools const&) noexcept = default;
-	constexpr component_pools(component_pools &&) noexcept = default;
-	constexpr component_pools& operator=(component_pools const&) noexcept = default;
-	constexpr component_pools& operator=(component_pools&&) noexcept = default;
-	constexpr ~component_pools() noexcept = default;
-
+struct component_pools : type_list_indices<transform_type<ComponentsList, modstrip_t>> {
 	explicit constexpr component_pools(auto... pools) noexcept : base_pools{pools...} {
 		Expects((pools != nullptr) && ...);
 	}
 
 	template <typename Component>
 	constexpr auto& get() const noexcept {
-		using T = std::remove_pointer_t<std::remove_cvref_t<reduce_parent_t<Component>>>;
+		using T = modstrip_t<Component>;
 		constexpr int index = component_pools::index_of(static_cast<impl::wrap_t<T>*>(nullptr));
 		return *static_cast<component_pool<T>*>(base_pools[index]);
 	}
