aaf0cfc618e1c1d01a65d6bac4eaa7d3d86e283d
Kenneth Gorking <kgorking@outlook.com>
2023-09-21 11:31:55 +0200

    Update gcc cmake commands in prep for release 14

--- cxx_modules_rules.cmake
+++ cxx_modules_rules.cmake
@@ -18,15 +18,13 @@ else()
 	#set(CMAKE_EXPERIMENTAL_CXX_MODULE_CMAKE_API "17be90bd-a850-44e0-be50-448de847d652") # ?
 endif()
 
-#
-# Set up compiler specific stuff
+# Set up GCC specific stuff
 if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
 	string(CONCAT CMAKE_EXPERIMENTAL_CXX_SCANDEP_SOURCE
-	  "<CMAKE_CXX_COMPILER> <DEFINES> <INCLUDES> <FLAGS> -E -x c++ <SOURCE>"
-	  " -MT <DYNDEP_FILE> -MD -MF <DEP_FILE>"
-	  " -fmodules-ts"
-	  #" -fdep-file=<DYNDEP_FILE> -fdep-output=<OBJECT> -fdep-format=trtbd" # requires patch
-	  " -o <PREPROCESSED_SOURCE>")
+      "<CMAKE_CXX_COMPILER> <DEFINES> <INCLUDES> <FLAGS> -E -x c++ <SOURCE>"
+      " -MT <DYNDEP_FILE> -MD -MF <DEP_FILE>"
+      " -fmodules-ts -fdeps-file=<DYNDEP_FILE> -fdeps-target=<OBJECT> -fdeps-format=p1689r5"
+      " -o <PREPROCESSED_SOURCE>")
 	set(CMAKE_EXPERIMENTAL_CXX_MODULE_MAP_FORMAT "gcc")
-	set(CMAKE_EXPERIMENTAL_CXX_MODULE_MAP_FLAG "-fmodules-ts -fmodule-mapper=<MODULE_MAP_FILE> -fdep-format=trtbd -x c++")
-endif ()
+	set(CMAKE_EXPERIMENTAL_CXX_MODULE_MAP_FLAG "-fmodules-ts -fmodule-mapper=<MODULE_MAP_FILE> -fdeps-format=p1689r5 -x c++")
+endif ()endif ()
