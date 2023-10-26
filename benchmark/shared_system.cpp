052a166e1e3b6d936815cfb01d8915e4dd5ba141
Kenneth Gorking <kgorking@outlook.com>
2023-10-26 15:28:53 +0200

    Added back missing function

---
+++ benchmark/shared_system.cpp
@@ -0,0 +1,29 @@
+#include <ecs/ecs.h>
+#include <complex>
+
+void benchmark_system(ecs::entity_id ent, int& color) noexcept {
+	constexpr int dimension = 16384;
+	constexpr int max_iterations = 50;
+	constexpr double fr_w = 1.5;
+	constexpr double fr_h = 1.5;
+	constexpr double fr_x = -2.2;
+	constexpr double fr_y = 1.2;
+
+	auto const x = ent % dimension;
+	auto const y = ent / dimension;
+
+	std::complex<double> c(static_cast<double>(x), static_cast<double>(y));
+
+	// Convert a pixel coordinate to the complex domain
+	c = {c.real() / (double)dimension * fr_w + fr_x, c.imag() / (double)dimension * fr_h + fr_y};
+
+	// Check if a point is in the set or escapes to infinity
+	std::complex<double> z(0);
+	int iter = 0;
+	while (abs(z) < 3 && iter < max_iterations) {
+		z = z * z + c;
+		iter++;
+	}
+
+	color = iter;
+}
