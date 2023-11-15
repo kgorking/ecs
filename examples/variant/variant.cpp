12698cb8a7aecce42acf45d613ff60dc99c03427
Kenneth Gorking <kgorking@outlook.com>
2023-11-15 13:53:24 +0100

    Extended comments

--- examples/variant/variant.cpp
+++ examples/variant/variant.cpp
@@ -1,8 +1,9 @@
 #include <ecs/ecs.h>
 #include <iostream>
 
-// Prints out 'ACB'.
-// Without the variants it would print 'AACACB'
+// Prints out 'A B C'.
+// Without the variants it would print 'A AB ABC'
+// If 'C' was also a variant of 'A', it prints 'A B BC'
 
 struct A { };
 struct B { using variant_of = A; };
@@ -18,12 +19,14 @@ int main() {
 	// Print 'A'
 	rt.add_component(0, A{});
 	rt.update();
-
-	// Print 'C'
-	rt.add_component(0, C{});
-	rt.update();
+	std::cout << ' ';
 
 	// Print 'B'
 	rt.add_component(0, B{});
+	rt.update();
+	std::cout << ' ';
+
+	// Print 'C'
+	rt.add_component(0, C{});
 	rt.update();
 }
