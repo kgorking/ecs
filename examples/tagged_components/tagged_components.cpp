c2299f067e4ebcded7a4805d357c8250ecd7570a
Kenneth Gorking <kgorking@outlook.com>
2023-11-21 19:14:10 +0100

    Spell-checked

--- examples/tagged_components/tagged_components.cpp
+++ examples/tagged_components/tagged_components.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 #include <string>
 
-struct flameable_t { using ecs_flags = ecs::flags<ecs::tag>; };
-struct freezeable_t { using ecs_flags = ecs::flags<ecs::tag>; };
+struct flammable_t { using ecs_flags = ecs::flags<ecs::tag>; };
+struct freezable_t { using ecs_flags = ecs::flags<ecs::tag>; };
 struct shockable_t { using ecs_flags = ecs::flags<ecs::tag>; };
 
 struct Name : std::string {};
@@ -13,34 +13,34 @@ int main() {
 
 	// Add systems for each tag. Will just print the name.
 	// Since tags never hold any data, they are always passed by value
-	rt.make_system<ecs::opts::group<0>>([](Name const &name, freezeable_t) { std::cout << "  freezeable: " << name << "\n"; });
+	rt.make_system<ecs::opts::group<0>>([](Name const &name, freezable_t) { std::cout << "  freezable: " << name << "\n"; });
 	rt.make_system<ecs::opts::group<1>>([](Name const &name, shockable_t) { std::cout << "  shockable:  " << name << "\n"; });
-	rt.make_system<ecs::opts::group<2>>([](Name const &name, flameable_t) { std::cout << "  flammeable: " << name << "\n"; });
+	rt.make_system<ecs::opts::group<2>>([](Name const &name, flammable_t) { std::cout << "  flammable: " << name << "\n"; });
 
 	// Set up the entities with a name and tags
-	rt.add_component(0, Name{"Jon"}, freezeable_t{});
-	rt.add_component(1, Name{"Sean"}, flameable_t{});
+	rt.add_component(0, Name{"Jon"}, freezable_t{});
+	rt.add_component(1, Name{"Sean"}, flammable_t{});
 	rt.add_component(2, Name{"Jimmy"}, shockable_t{});
-	rt.add_component(3, Name{"Rachel"}, flameable_t{}, freezeable_t{}, shockable_t{});
-	rt.add_component(4, Name{"Suzy"}, flameable_t{});
+	rt.add_component(3, Name{"Rachel"}, flammable_t{}, freezable_t{}, shockable_t{});
+	rt.add_component(4, Name{"Suzy"}, flammable_t{});
 
 	// Commit the changes
 	rt.commit_changes();
-	std::cout << "Created 'Jon' with freezeable_t\n"
-				 "        'Sean' with flameable_t\n"
+	std::cout << "Created 'Jon' with freezable_t\n"
+				 "        'Sean' with flammable_t\n"
 				 "        'Jimmy' with shockable_t\n"
-				 "        'Rachel' with flameable_t, freezeable_t, shockable_t\n"
-				 "        'Suzy' with flameable_t\n\n";
+				 "        'Rachel' with flammable_t, freezable_t, shockable_t\n"
+				 "        'Suzy' with flammable_t\n\n";
 
 	// Run the systems
 	std::cout << "Running systems:\n";
 	rt.run_systems();
 
 	std::cout << "\nStat dump:\n";
-	std::cout << "  Number of entities with the flameable_t tag:  " << rt.get_entity_count<flameable_t>()
+	std::cout << "  Number of entities with the flammable_t tag:  " << rt.get_entity_count<flammable_t>()
 			  << "\n  Number of entities with the shockable_t tag:  " << rt.get_entity_count<shockable_t>()
-			  << "\n  Number of entities with the freezeable_t tag: " << rt.get_entity_count<freezeable_t>()
-			  << "\n  Number of flameable_t components allocated:   " << rt.get_component_count<flameable_t>()
+			  << "\n  Number of entities with the freezable_t tag: " << rt.get_entity_count<freezable_t>()
+			  << "\n  Number of flammable_t components allocated:   " << rt.get_component_count<flammable_t>()
 			  << "\n  Number of shockable_t components allocated:   " << rt.get_component_count<shockable_t>()
-			  << "\n  Number of freezeable_t components allocated:  " << rt.get_component_count<freezeable_t>() << "\n";
+			  << "\n  Number of freezable_t components allocated:  " << rt.get_component_count<freezable_t>() << "\n";
 }
