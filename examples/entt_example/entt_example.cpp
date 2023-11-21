1732ed60a44373c194ddc2ed89a7ebb703701f23
Kenneth Gorking <kgorking@outlook.com>
2023-11-21 17:45:23 +0100

    Updated entt example, for some reason

--- examples/entt_example/entt_example.cpp
+++ examples/entt_example/entt_example.cpp
@@ -14,13 +14,20 @@ struct velocity {
 	float dy;
 };
 
+template<typename T>
+T generator(ecs::entity_id id) {
+	auto const fid = (float)id;
+	return {fid * .1f, fid * .1f};
+}
+
 int main() {
 	ecs::runtime rt;
 
 	rt.make_system([](ecs::entity_id /*id*/, position& /*pos*/, velocity const& /*vel*/) { /* ... */ });
 	rt.make_system([](velocity& /*vel*/) { /* ... */ });
 
-	rt.add_component({0, 9}, position{}, velocity{});
+	rt.add_component_generator({0, 9}, generator<position>);
+	rt.add_component_generator({0, 4}, generator<velocity>);
 
 	rt.update();
 }
