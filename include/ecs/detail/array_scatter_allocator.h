c608ea602f31269386d036e7408fb023f04b1963
Kenneth Gorking <kgorking@outlook.com>
2024-03-19 19:20:40 +0100

    Allocator basics done

--- include/ecs/detail/array_scatter_allocator.h
+++ include/ecs/detail/array_scatter_allocator.h
@@ -1,79 +1,182 @@
 #ifndef ECS_DETAIL_ARRAY_SCATTER_ALLOCATOR_H
 #define ECS_DETAIL_ARRAY_SCATTER_ALLOCATOR_H
 
+#include "contract.h"
 #include <memory>
-#include <cstdlib>
-#include <forward_list>
+#include <span>
 
 namespace ecs::detail {
-	template<typename T>
+	template <typename Fn, typename T>
+	concept callback_takes_a_span = std::invocable<Fn, std::span<T>>;
+
+	// the 'Array Scatter Allocator'.
+	// * Optimized for allocating arrays of objects, instead of one-at-the-time
+	//   that the stl allocators do.
+	// * A single allocation can result in many addresses being returned, as the
+	//   allocator fills in holes in the internal pools of memory.
+	template <typename T, int DefaultStartingSize = 16>
 	struct array_scatter_allocator {
-		array_scatter_allocator() {
-			add_pool(16);
+		static_assert(DefaultStartingSize > 0);
+
+		template <typename... Ts>
+		constexpr std::vector<std::span<T>> allocate(int const count, Ts&&... args) {
+			std::vector<std::span<T>> r;
+			allocate_with_callback(
+				count,
+				[&r](std::span<T> s) {
+					r.push_back(s);
+				},
+				std::forward<Ts>(args)...);
+			return r;
 		}
 
-		~array_scatter_allocator() {
-			for (pool& p : pools) {
-				std::destroy_n(p.base, p.capacity);
-				std::free(p.base);
-			}
-		}
-
-		template<typename ...Ts>
-		int allocate(int count, auto&& alloc_callback, Ts&& ...args) {
+		template <typename... Ts>
+		constexpr int allocate_with_callback(int const count, callback_takes_a_span<T> auto&& alloc_callback, Ts&&... args) {
 			int splits = 0;
-			auto it = pools.begin();
-			while(count > 0) {
-				if (it == pools.end()) {
-					add_pool(pools.front().capacity << 1);
-					it = pools.begin();
+			int remaining_count = count;
+
+			// Take space from free list
+			std::unique_ptr<free_block>* ptr_free = &free_list;
+			while (remaining_count > 0 && *ptr_free) {
+				free_block* ptr = ptr_free->get();
+				int const min_space = std::min(remaining_count, (int)(ptr->span.size()));
+				if (min_space == 0) {
+					ptr_free = &ptr->next;
+					continue;
 				}
 
-				pool& p = *it++;
+				std::span<T> span = ptr->span.subspan(0, min_space);
+				for (T& val : span)
+					val = {args...};
 
-				int const min_space = std::min(count, p.unused);
+				splits += 1;
+				remaining_count -= min_space;
+
+				if (min_space == (int)ptr->span.size()) {
+					*ptr_free = std::move(ptr->next);
+				} else {
+					ptr->span = ptr->span.subspan(min_space + 1);
+					ptr_free = &ptr->next;
+				}
+			}
+
+			// Take space from pool(s)
+			pool* ptr_pool = pools.get();
+			while (remaining_count > 0) {
+				if (ptr_pool == nullptr)
+					ptr_pool = add_pool(pools ? pools->capacity << 1 : DefaultStartingSize);
+
+				pool& p = *ptr_pool;
+				ptr_pool = ptr_pool->next.get();
+
+				unsigned const min_space = std::min(remaining_count, p.unused);
 				if (min_space == 0)
 					continue;
 
 				splits += 1;
-				count -= min_space;
+				remaining_count -= min_space;
 				p.unused -= min_space;
 
-				T* ptr = p.base + p.next_available;
+				T* ptr = p.base.get() + p.next_available;
 				p.next_available += min_space;
 
-				for(int i=0; i<min_space; i++)
-					std::construct_at(ptr + i, std::forward<Ts>(args)...);
-				alloc_callback(ptr, min_space);
+				for (unsigned i = 0; i < min_space; i++)
+					ptr[i] = {args...};
+
+				alloc_callback(std::span<T>{ptr, min_space});
 			}
+
 			return splits;
 		}
 
-		void deallocate(T* ptr, int count) {
-			std::destroy_n(ptr, count);
-			free_list.emplace_front(ptr, count);
+		constexpr void deallocate(std::span<T> const span) {
+			PreAudit(validate_addr(span), "Invalid address passed to deallocate()");
+			free_list = std::make_unique<free_block>(span, std::move(free_list));
 		}
 
 	private:
-		void add_pool(int size) {
-			pools.emplace_front(size, size, (T*)std::calloc(size, sizeof(T)), 0);
+		constexpr auto* add_pool(int const size) {
+			pools = std::make_unique<pool>(std::move(pools), std::make_unique_for_overwrite<T[]>(size), size, size, 0);
+			return pools.get();
+		}
+
+		constexpr bool validate_addr(std::span<T> const span) {
+			auto* p = pools.get();
+			while (p != nullptr) {
+				T const* const begin = p->base.get();
+				T const* const end = begin + p->capacity;
+				if (span.data() >= begin && (span.data() + span.size()) < end)
+					return true;
+				p = p->next.get();
+			}
+			return false;
 		}
 
 		struct free_block {
-			T* ptr;
-			int count;
+			std::span<T> span;
+			std::unique_ptr<free_block> next;
 		};
 
 		struct pool {
+			std::unique_ptr<pool> next;
+			std::unique_ptr<T[]> base;
 			int unused;
 			int capacity;
-			T* base;
 			int next_available;
 		};
 
-		std::forward_list<pool> pools;
-		std::forward_list<free_block> free_list;
+		std::unique_ptr<pool> pools;
+		std::unique_ptr<free_block> free_list;
 	};
-}
+
+	// UNIT TESTS
+	static_assert(
+		[] {
+			constexpr std::size_t elems_to_alloc = 12'345;
+			array_scatter_allocator<int> alloc;
+			std::size_t total_alloc = 0;
+			alloc.allocate_with_callback(elems_to_alloc, [&](std::span<int> s) {
+				total_alloc += s.size();
+			});
+			return elems_to_alloc == total_alloc;
+		}(),
+		"Array-scatter allocator allocates correctly");
+
+	static_assert(
+		[] {
+			array_scatter_allocator<int> alloc;
+
+			int sum = 0;
+			alloc.allocate_with_callback(
+				10,
+				[&](std::span<int> s) {
+					for (int val : s)
+						sum += val;
+				},
+				5);
+			return 50 == sum;
+		}(),
+		"Array-scatter allocator initializes correctly");
+
+	static_assert(
+		[] {
+			array_scatter_allocator<int> alloc;
+			std::vector<std::span<int>> r = alloc.allocate(10);
+			auto const subspan = r[0].subspan(3, 4);
+			alloc.deallocate(subspan);
+			return true;
+		},
+		"Array-scatter allocator frees correctly");
+
+	static_assert(
+		[] {
+			array_scatter_allocator<int> alloc;
+			auto vec = alloc.allocate(10);			// +10
+			alloc.deallocate(vec[0].subspan(3, 4)); // -4
+			vec = alloc.allocate(20);				// +20
+			return true;
+		},
+		"Array-scatter allocator scatters correctly");
+} // namespace ecs::detail
 
 #endif // !ECS_DETAIL_ARRAY_SCATTER_ALLOCATOR_H
--- unittest/array_scatter_allocator.cpp
+++ unittest/array_scatter_allocator.cpp
@@ -5,12 +5,7 @@ using ecs::detail::array_scatter_allocator;
 
 TEST_CASE("Array-scatter allocator") {
 	array_scatter_allocator<int> alloc;
-
-	int total_alloc = 0;
-	int const pools_used = alloc.allocate(24, [&](int* , int count) {
-		total_alloc += count;
-	});
-
-	REQUIRE(total_alloc == 24);
-	REQUIRE(pools_used == 2);
+	auto vec = alloc.allocate(10);			// +10
+	alloc.deallocate(vec[0].subspan(3, 4)); // -4
+	vec = alloc.allocate(20);				// +20
 }
