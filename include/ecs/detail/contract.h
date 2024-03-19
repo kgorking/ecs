ba48d856b4b2b6d400ddfd91a09278d7c2e2c278
Kenneth Gorking <kgorking@outlook.com>
2024-03-19 23:02:01 +0100

    Made contacts useable in compile time contexts

--- include/ecs/detail/contract.h
+++ include/ecs/detail/contract.h
@@ -3,90 +3,89 @@
 
 #include <concepts>
 #include <iostream>
+#include <utility>
 #if __has_include(<stacktrace>)
 #include <stacktrace>
 #endif
 
 // Contracts. If they are violated, the program is in an invalid state and is terminated.
 namespace ecs::detail {
-// Concept for the contract violation interface.
-template <typename T>
-concept contract_violation_interface = requires(T t) {
-	{ t.assertion_failed("", "") } -> std::same_as<void>;
-	{ t.precondition_violation("", "") } -> std::same_as<void>;
-	{ t.postcondition_violation("", "") } -> std::same_as<void>;
-};
+	// Concept for the contract violation interface.
+	template <typename T>
+	concept contract_violation_interface = requires(T t) {
+		{ t.assertion_failed("", "") } -> std::same_as<void>;
+		{ t.precondition_violation("", "") } -> std::same_as<void>;
+		{ t.postcondition_violation("", "") } -> std::same_as<void>;
+	};
 
-struct default_contract_violation_impl {
-	void panic(char const* why, char const* what, char const* how) noexcept {
-		std::cerr << why << ": \"" << how << "\"\n\t" << what << "\n\n";
+	struct default_contract_violation_impl {
+		void panic(char const* why, char const* what, char const* how) noexcept {
+			std::cerr << why << ": \"" << how << "\"\n\t" << what << "\n\n";
 #ifdef __cpp_lib_stacktrace
-		// Dump a stack trace if available
-		std::cerr << "** stack dump **\n" << std::stacktrace::current(3) << '\n';
+			// Dump a stack trace if available
+			std::cerr << "** stack dump **\n" << std::stacktrace::current(3) << '\n';
 #endif
-		std::terminate();
-	}
+			std::terminate();
+		}
 
-	void assertion_failed(char const* what, char const* how) noexcept {
-		panic("Assertion failed", what, how);
-	}
+		void assertion_failed(char const* what, char const* how) noexcept {
+			panic("Assertion failed", what, how);
+		}
 
-	void precondition_violation(char const* what, char const* how) noexcept {
-		panic("Precondition violation", what, how);
-	}
+		void precondition_violation(char const* what, char const* how) noexcept {
+			panic("Precondition violation", what, how);
+		}
 
-	void postcondition_violation(char const* what, char const* how) noexcept {
-		panic("Postcondition violation", what, how);
-	}
-};
+		void postcondition_violation(char const* what, char const* how) noexcept {
+			panic("Postcondition violation", what, how);
+		}
+	};
 } // namespace ecs::detail
 
 // The contract violation interface, which can be overridden by users
 ECS_EXPORT namespace ecs {
-template <typename...>
-auto contract_violation_handler = ecs::detail::default_contract_violation_impl{};
+	template <typename...>
+	auto contract_violation_handler = ecs::detail::default_contract_violation_impl{};
 }
 
 #if ECS_ENABLE_CONTRACTS
 
 namespace ecs::detail {
-template <typename... DummyArgs>
-	requires(sizeof...(DummyArgs) == 0)
-inline void do_assertion_failed(char const* what, char const* how) {
-	ecs::detail::contract_violation_interface auto& cvi = ecs::contract_violation_handler<DummyArgs...>;
-	cvi.assertion_failed(what, how);
-}
+	template <typename... DummyArgs>
+		requires(sizeof...(DummyArgs) == 0)
+	inline void do_assertion_failed(char const* what, char const* how) {
+		ecs::detail::contract_violation_interface auto& cvi = ecs::contract_violation_handler<DummyArgs...>;
+		cvi.assertion_failed(what, how);
+	}
 
-template <typename... DummyArgs>
-	requires(sizeof...(DummyArgs) == 0)
-inline void do_precondition_violation(char const* what, char const* how) {
-	ecs::detail::contract_violation_interface auto& cvi = ecs::contract_violation_handler<DummyArgs...>;
-	cvi.precondition_violation(what, how);
-}
+	template <typename... DummyArgs>
+		requires(sizeof...(DummyArgs) == 0)
+	inline void do_precondition_violation(char const* what, char const* how) {
+		ecs::detail::contract_violation_interface auto& cvi = ecs::contract_violation_handler<DummyArgs...>;
+		cvi.precondition_violation(what, how);
+	}
 
-template <typename... DummyArgs>
-	requires(sizeof...(DummyArgs) == 0)
-inline void do_postcondition_violation(char const* what, char const* how) {
-	ecs::detail::contract_violation_interface auto& cvi = ecs::contract_violation_handler<DummyArgs...>;
-	cvi.postcondition_violation(what, how);
-}
+	template <typename... DummyArgs>
+		requires(sizeof...(DummyArgs) == 0)
+	inline void do_postcondition_violation(char const* what, char const* how) {
+		ecs::detail::contract_violation_interface auto& cvi = ecs::contract_violation_handler<DummyArgs...>;
+		cvi.postcondition_violation(what, how);
+	}
 } // namespace ecs::detail
 
-
-#define Assert(expression, message)                                                                                                        \
+#define ECS_InvokeContractBreach(expression, message, func)                                                                                \
 	do {                                                                                                                                   \
-		((expression) ? static_cast<void>(0) : ecs::detail::do_assertion_failed(#expression, message));                                    \
+		if (!(expression)) {                                                                                                               \
+			if (std::is_constant_evaluated())                                                                                              \
+				std::unreachable();                                                                                                        \
+			else                                                                                                                           \
+				func(#expression, message);                                                        \
+		}                                                                                                                                  \
 	} while (false)
 
-#define Pre(expression, message)                                                                                                           \
-	do {                                                                                                                                   \
-		((expression) ? static_cast<void>(0) : ecs::detail::do_precondition_violation(#expression, message));                              \
-	} while (false)
-
-#define Post(expression, message)                                                                                                          \
-	do {                                                                                                                                   \
-		((expression) ? static_cast<void>(0) : ecs::detail::do_postcondition_violation(#expression, message));                             \
-	} while (false)
+#define Assert(expression, message) ECS_InvokeContractBreach(expression, message, ecs::detail::do_assertion_failed)
+#define Pre(expression, message) ECS_InvokeContractBreach(expression, message, ecs::detail::do_precondition_violation)
+#define Post(expression, message) ECS_InvokeContractBreach(expression, message, ecs::detail::do_postcondition_violation)
 
 // Audit contracts. Used for expensive checks; can be disabled.
 #if ECS_ENABLE_CONTRACTS_AUDIT
